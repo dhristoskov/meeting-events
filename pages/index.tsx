729c0cc158db379ba417d1b75a69f8f2d476d6b1
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2021-03-10 18:48:10 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import HomeComponent from '@/components/home-component/HomeComponent';
+
+const Home = () =>  {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  )
+}
+
+export default Home;
